44afc16eb300ae96cb97ddb3d5db70c5e288eca4
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-11-11 17:17:42 +0000

    updated src/js/index.js component

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Calculator from "./component/calculator.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Calculator />, document.querySelector("#app"));
