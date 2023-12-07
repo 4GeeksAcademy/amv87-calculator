ba9858473b8b91dc75666db9e2fe30e2bf8335fa
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-12-07 11:35:22 +0000

    finished layout

--- src/js/component/calculator.jsx
+++ src/js/component/calculator.jsx
@@ -8,31 +8,31 @@ const Calculator = () => {
                     <div className="m-0 my-2 row d-flex ustify-content-center rounded-3 mt-3 mx-1 text-end text-white">
                         <div className="calculator-screen rounded-3 display-2">9876</div>
                     </div>
-                    <div className="keys-row1 mx-0 my-3 row d-flex justify-content-evenly text-center text-white">
-                        <input type="button" value="C" className="key rounded-3 display-5" />
-                        <div className="key rounded-3 display-5">+-</div>
+                    <div className="keys-row1 mx-0 my-3 row d-flex justify-content-evenly text-center">
+                        <button type="button" value="C" className="key rounded-3 display-5">C</button>
+                        <button className="key rounded-3 display-5">+-</button>
                         <div className="key rounded-3 display-5">%</div>
                         <div className="key rounded-3 display-5">/</div>
                     </div>
-                    <div className="keys-row2 mx-0 my-3 row d-flex justify-content-evenly text-center text-white">
-                        <div className="key rounded-3 display-5">7</div>
+                    <div className="keys-row2 mx-0 my-3 row d-flex justify-content-evenly text-center">
+                        <input type="button" value="7" className="key rounded-3 display-5" />
                         <div className="key rounded-3 display-5">8</div>
                         <div className="key rounded-3 display-5">9</div>
                         <div className="key rounded-3 display-5">x</div>
                     </div>
-                    <div className="keys-row3 mx-0 my-3 row d-flex justify-content-evenly text-center text-white">
+                    <div className="keys-row3 mx-0 my-3 row d-flex justify-content-evenly text-center">
                         <div className="key rounded-3 display-5">4</div>
                         <div className="key rounded-3 display-5">5</div>
                         <div className="key rounded-3 display-5">6</div>
                         <div className="key rounded-3 display-5">-</div>
                     </div>
-                    <div className="keys-row4 mx-0 my-3 row d-flex justify-content-evenly text-center text-white">
+                    <div className="keys-row4 mx-0 my-3 row d-flex justify-content-evenly text-center">
                         <div className="key rounded-3 display-5">1</div>
                         <div className="key rounded-3 display-5">2</div>
                         <div className="key rounded-3 display-5">3</div>
                         <div className="key rounded-3 display-5">+</div>
                     </div>
-                    <div className="keys-row5 mx-0 my-3 row d-flex justify-content-evenly text-center text-white">
+                    <div className="keys-row5 mx-0 my-3 row d-flex justify-content-evenly text-center">
                         <div className="key rounded-3 display-5">0</div>
                         <div className="key rounded-3 display-5">.</div>
                         <div className="key-equal rounded-3 display-5">=</div>
